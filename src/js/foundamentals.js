20d146aff16223c3feb1286de245f7bbf32d563a
murlloy <minguerilogenshin@gmail.com>
2024-12-10 17:08:19 -0300

    feat: aula do luquinhas correaYT

---
+++ src/js/foundamentals.js
@@ -0,0 +1,41 @@
+
+// variaveis em javascript
+
+let chocolate = 'tete'
+const banana = "banana"
+const morango = "morango"
+const feminino = 'Feminino'
+var humano = "homi"
+
+// variable no-binario
+let maria
+
+// console by luquinhas correa
+
+console.log(chocolate)
+console.error(banana)
+console.info("Sou uma informacao")
+console.warn("Var é depressão")
+
+console.log(maria)
+
+// objeto estranho....... by luquinhas correa 2025
+
+let pessoa = {
+
+    nome: "maria",     // atributo
+    idade: 17,         // atributo
+    sexo: feminino     // atribute
+
+}
+
+
+// vector!!!!!!!!!! frutas.
+const frutas = [banana, morango]
+console.table(frutas)
+console.table(pessoa)
+
+
+
+
+
